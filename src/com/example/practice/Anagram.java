8004f5dc6be5c16fc7cde6999de32ea6c975c0b2
reddykumarmv@gmail.com <RShiva@27>
2017-06-19 21:08:31 +0200

    Anagrams in Java

--- src/com/example/practice/Anagram.java
+++ src/com/example/practice/Anagram.java
@@ -10,20 +10,25 @@ public class Anagram {
     static public void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        //Reading input from the console
         System.out.println("Enter first string:-");
        String first = input.nextLine();
         System.out.println("Enter second string:-");
        String second = input.nextLine();
+       //Converting the strings to lowercase.
        first= first.toLowerCase();
        second= second.toLowerCase();
+       //Anagrams should have equal length
        if( first.length() == second.length())
        {
+           //To sort Arrays we need to convert to Char Arrays
            char[] first_char = first.toCharArray();
            char[] second_char = second.toCharArray();
            Arrays.sort(first_char);
            Arrays.sort(second_char);
            String first_sorted = new String(first_char);
            String second_sorted = new String(second_char);
+           //Comparing sorted Arrays
            if(first_sorted.equals(second_sorted))
            {
                System.out.println("Strings are Anagrams");
